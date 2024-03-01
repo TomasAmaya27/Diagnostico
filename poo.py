18fb73e639b3b6a636089ca6b261659aa510efee
Tomasamaya <133403717+TomasAmaya27@users.noreply.github.com>
2024-03-01 10:50:55 -0300

    Create poo.py

---
+++ poo.py
@@ -0,0 +1,17 @@
+#Crear una clase llamada vehiculo capaz de procesar la informacion basica de los autos en una concecionaria debera tener constructor y los atributos de el vehiculo seran: patente,marca,modelo,kilometraje. crear metodos (de acceso) getter y (modificacion) setter. Mostrar en pantalla al menos un atributo y modificar el kilometraje
+
+class vehiculo:
+  def __int__ (self, patente, marca, modelo, kilometraje):
+    self.patente = patente
+    self.marca = marca
+    self.modelo = modelo
+    self.kiometraje = kilometraje
+
+def get_patente (self):
+  return self.patente
+
+def get_marca (self):
+  return self.marca
+
+def get_modelo (self)+
+return self.modelo
